76e939654aafa303e1edfb700b19afbfd2c46f72
akibsayyed <akibsayyed@gmail.com>
2023-07-07 12:10:39 +0530

    Update authorize.c
    Updated Algo

--- src/modules/ims_auth/authorize.c
+++ src/modules/ims_auth/authorize.c
@@ -547,7 +547,7 @@ int www_resync_auth(struct sip_msg* msg, char* _route, char* str1, char* str2) {
         return CSCF_RETURN_BREAK;
     }
 
-    algo_type = registration_default_algorithm_type;
+    algo_type = get_algorithm_type({"AKAv1-MD5", 9});
 
     /* check if it is a synchronization request */
     //TODO this is MAR syncing - have removed it currently - TOD maybe put back in
